456c81d2a29aa7266c8cf79efd6c53430c2b87cd
jeongseok-han <jeongseok.han@nhn.com>
2016-06-29 13:10:27 +0900

    init project

---
+++ ChatApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace ChatApp
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new ChatAppPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ ChatApp/ChatAppPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace ChatApp
+{
+	public partial class ChatAppPage : ContentPage
+	{
+		public ChatAppPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
